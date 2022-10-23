a1dc54cf019a291d15e61d369dd4b7d8ba0a1b99
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-10-23 17:28:04 +0200

    MenuService & MenuValidator for Menu

--- src/main/java/groupId/artifactId/service/api/IMenuValidator.java
+++ src/main/java/groupId/artifactId/service/api/IMenuValidator.java
@@ -1,11 +1,13 @@
 package groupId.artifactId.service.api;
 
+import groupId.artifactId.core.dto.MenuDto;
 import groupId.artifactId.core.dto.MenuItemDto;
 import groupId.artifactId.core.dto.MenuItemDtoWithId;
 
 import java.util.List;
 
 public interface IMenuValidator {
-    void validateMenu(List<MenuItemDto> menuItemDto);
+    void validateListMenuItems(List<MenuItemDto> menuItemDto);
     void validateMenuItem(MenuItemDtoWithId menuItemDtoWithId);
+    void validateMenu(MenuDto menuDto);
 }
