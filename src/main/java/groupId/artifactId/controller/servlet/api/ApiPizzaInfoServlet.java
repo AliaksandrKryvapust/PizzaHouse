dbba80c74697d5b1fe952bed8ca40cfd5d355d17
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-11-02 14:38:51 +0100

    Servlet for PizzaInfo was changed

--- src/main/java/groupId/artifactId/controller/servlet/api/ApiPizzaInfoServlet.java
+++ src/main/java/groupId/artifactId/controller/servlet/api/ApiPizzaInfoServlet.java
@@ -1,9 +1,10 @@
 package groupId.artifactId.controller.servlet.api;
 
-import groupId.artifactId.exceptions.IncorrectEncodingException;
-import groupId.artifactId.exceptions.IncorrectServletInputStreamException;
-import groupId.artifactId.exceptions.IncorrectServletWriterException;
-import groupId.artifactId.service.PizzaInfoService;
+import groupId.artifactId.controller.validator.IoC.PizzaInfoValidatorSingleton;
+import groupId.artifactId.controller.validator.api.IPizzaInfoValidator;
+import groupId.artifactId.core.dto.input.PizzaInfoDtoInput;
+import groupId.artifactId.core.dto.output.PizzaInfoDtoOutput;
+import groupId.artifactId.service.IoC.PizzaInfoServiceSingleton;
 import groupId.artifactId.service.api.IPizzaInfoService;
 import groupId.artifactId.utils.JsonConverter;
 
@@ -11,44 +12,45 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 
 //CRUD controller
 //IPizzaInfo
-@WebServlet(name = "PizzaInfo", urlPatterns = "/api/menu/item/pizza_info")
+@WebServlet(name = "PizzaInfo", urlPatterns = "/api/pizza_info")
 public class ApiPizzaInfoServlet extends HttpServlet {
-    private final IPizzaInfoService pizzaInfoService = PizzaInfoService.getInstance();
+    private final IPizzaInfoService pizzaInfoService = PizzaInfoServiceSingleton.getInstance();
+    private final IPizzaInfoValidator pizzaInfoValidator = PizzaInfoValidatorSingleton.getInstance();
+    private static final String CONTENT_TYPE = "application/json";
+    private static final String ENCODING = "UTF-8";
+    private static final String PARAMETER_ID = "id";
+    private static final String PARAMETER_VERSION = "version";
+    private static final String PARAMETER_DELETE = "delete";
 
     //Read POSITION
     //1) Read list
-    //2) Read item need id param  (id = 95)
+    //2) Read item need id param  (id = 122)
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         try {
-            resp.setContentType("application/json");
-            resp.setCharacterEncoding("UTF-8");
-            String id = req.getParameter("id");
+            resp.setContentType(CONTENT_TYPE);
+            resp.setCharacterEncoding(ENCODING);
+            String id = req.getParameter(PARAMETER_ID);
             if (id != null) {
                 if (pizzaInfoService.isIdValid(Long.valueOf(id))) {
                     resp.getWriter().write(JsonConverter.fromPizzaInfoToJson(pizzaInfoService.get(Long.valueOf(id))));
                 } else {
-                    resp.setStatus(400);
-                    throw new IllegalArgumentException("PizzaInfo id is not exist");
+                    resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
                 }
             } else {
                 resp.getWriter().write(JsonConverter.fromPizzaInfoListToJson(pizzaInfoService.get()));
             }
-        } catch (IOException e) {
-            resp.setStatus(500);
-            throw new IncorrectServletWriterException("Incorrect servlet state during response writer method", e);
+        } catch (Exception e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
-        resp.setStatus(200);
+        resp.setStatus(HttpServletResponse.SC_OK);
     }
 
     //CREATE POSITION
     //body json
-    //to add new PizzaInfo in Storage
 //   {
 //           "name":"ITALIANO PIZZA",
 //           "description":"Mozzarella cheese, basilica, ham",
@@ -57,17 +59,24 @@ public class ApiPizzaInfoServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
         try {
-            req.setCharacterEncoding("UTF-8");
-            resp.setContentType("application/json");
-            pizzaInfoService.save(JsonConverter.fromJsonToPizzaInfo(req.getInputStream()));
-        } catch (UnsupportedEncodingException e) {
-            resp.setStatus(500);
-            throw new IncorrectEncodingException("Failed to set character encoding UTF-8", e);
-        } catch (IOException e) {
-            resp.setStatus(500);
-            throw new IncorrectServletInputStreamException("Impossible to get input stream from request", e);
+            resp.setCharacterEncoding(ENCODING);
+            resp.setContentType(CONTENT_TYPE);
+            PizzaInfoDtoInput pizzaInfo = JsonConverter.fromJsonToPizzaInfo(req.getInputStream());
+            if (!pizzaInfoService.exist(pizzaInfo.getName())) {
+                try {
+                    pizzaInfoValidator.validate(pizzaInfo);
+                } catch (IllegalArgumentException e) {
+                    resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                }
+                PizzaInfoDtoOutput pizzaInfoDto = pizzaInfoService.save(pizzaInfo);
+                resp.getWriter().write(JsonConverter.fromPizzaInfoToJson(pizzaInfoDto));
+            } else {
+                resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            }
+        } catch (Exception e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
-        resp.setStatus(201);
+        resp.setStatus(HttpServletResponse.SC_CREATED);
     }
     //UPDATE POSITION
     //need param id  (id = 97)
@@ -81,55 +90,55 @@ public class ApiPizzaInfoServlet extends HttpServlet {
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) {
         try {
-            req.setCharacterEncoding("UTF-8");
-            resp.setContentType("application/json");
-            String id = req.getParameter("id");
-            String version = req.getParameter("version");
-            if (id!=null && version!=null){
+            resp.setCharacterEncoding(ENCODING);
+            resp.setContentType(CONTENT_TYPE);
+            String id = req.getParameter(PARAMETER_ID);
+            String version = req.getParameter(PARAMETER_VERSION);
+            if (id != null && version != null) {
                 if (pizzaInfoService.isIdValid(Long.valueOf(id))) {
-                    pizzaInfoService.update(JsonConverter.fromJsonToPizzaInfoUpdate(req.getInputStream()), id, version);
+                    PizzaInfoDtoInput pizzaInfo = JsonConverter.fromJsonToPizzaInfo(req.getInputStream());
+                    try {
+                        pizzaInfoValidator.validate(pizzaInfo);
+                    } catch (IllegalArgumentException e) {
+                        resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                    }
+                    PizzaInfoDtoOutput pizzaInfoDto = pizzaInfoService.update(pizzaInfo, id, version);
+                    resp.getWriter().write(JsonConverter.fromPizzaInfoToJson(pizzaInfoDto));
                 } else {
-                    resp.setStatus(400);
-                    throw new IllegalArgumentException("PizzaInfo id is not exist");
+                    resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
                 }
             } else {
-                resp.setStatus(400);
-                throw new IllegalArgumentException("Field PizzaInfo id or PizzaInfo version is empty");
+                resp.setStatus(HttpServletResponse.SC_PRECONDITION_FAILED);
             }
-        } catch (UnsupportedEncodingException e) {
-            resp.setStatus(500);
-            throw new IncorrectEncodingException("Failed to set character encoding UTF-8", e);
-        } catch (IOException e) {
-            resp.setStatus(500);
-            throw new IncorrectServletInputStreamException("Impossible to get input stream from request", e);
+        } catch (Exception e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
-        resp.setStatus(201);
+        resp.setStatus(HttpServletResponse.SC_CREATED);
     }
     //DELETE POSITION
     //need param id  (id = 97)
     //need param version/date_update - optimistic lock (version=2)
+    //param delete - true/false completely delete (delete=false)
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) {
         try {
-            req.setCharacterEncoding("UTF-8");
-            resp.setContentType("application/json");
-            String id = req.getParameter("id");
-            String version = req.getParameter("version");
-            if (id!=null && version!=null){
+            resp.setCharacterEncoding(ENCODING);
+            resp.setContentType(CONTENT_TYPE);
+            String id = req.getParameter(PARAMETER_ID);
+            String version = req.getParameter(PARAMETER_VERSION);
+            String delete = req.getParameter(PARAMETER_DELETE);
+            if (id != null && version != null && delete != null) {
                 if (pizzaInfoService.isIdValid(Long.valueOf(id))) {
-                    pizzaInfoService.delete(id,version);
+                    pizzaInfoService.delete(id, version, delete);
                 } else {
-                    resp.setStatus(400);
-                    throw new IllegalArgumentException("PizzaInfo id is not exist");
+                    resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
                 }
             } else {
-                resp.setStatus(400);
-                throw new IllegalArgumentException("Field PizzaInfo id or PizzaInfo version is empty");
+                resp.setStatus(HttpServletResponse.SC_PRECONDITION_FAILED);
             }
-        } catch (UnsupportedEncodingException e) {
-            resp.setStatus(500);
-            throw new IncorrectEncodingException("Failed to set character encoding UTF-8", e);
+        } catch (Exception e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
-        resp.setStatus(200);
+        resp.setStatus(HttpServletResponse.SC_OK);
     }
 }
