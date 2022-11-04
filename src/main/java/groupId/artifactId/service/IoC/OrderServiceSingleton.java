2b3a88784e8e86147c61dd601183b085f4a99cc3
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-11-04 17:43:08 +0100

    IoC service for order

---
+++ src/main/java/groupId/artifactId/service/IoC/OrderServiceSingleton.java
@@ -0,0 +1,26 @@
+package groupId.artifactId.service.IoC;
+
+import groupId.artifactId.dao.IoC.OrderDaoSingleton;
+import groupId.artifactId.dao.IoC.SelectedItemDaoSingleton;
+import groupId.artifactId.dao.IoC.TicketDaoSingleton;
+import groupId.artifactId.service.OrderService;
+import groupId.artifactId.service.api.IOrderService;
+
+public class OrderServiceSingleton {
+    private final IOrderService orderService;
+    private volatile static OrderServiceSingleton instance = null;
+
+    public OrderServiceSingleton() {
+        this.orderService = new OrderService(SelectedItemDaoSingleton.getInstance(), OrderDaoSingleton.getInstance(),
+                TicketDaoSingleton.getInstance());
+    }
+
+    public static IOrderService getInstance() {
+        synchronized (OrderServiceSingleton.class) {
+            if (instance == null) {
+                instance = new OrderServiceSingleton();
+            }
+        }
+        return instance.orderService;
+    }
+}
