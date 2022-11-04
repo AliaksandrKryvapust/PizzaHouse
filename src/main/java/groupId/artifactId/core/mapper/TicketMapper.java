dcb4d0a2831c1d607bc4e3bdb2612d638bb807bf
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-11-04 19:02:53 +0100

    new ticket mapper

---
+++ src/main/java/groupId/artifactId/core/mapper/TicketMapper.java
@@ -0,0 +1,13 @@
+package groupId.artifactId.core.mapper;
+
+import groupId.artifactId.core.dto.output.OrderDtoOutput;
+import groupId.artifactId.core.dto.output.TicketDtoOutPut;
+import groupId.artifactId.dao.entity.api.ITicket;
+
+public class TicketMapper {
+    public static TicketDtoOutPut ticketOutputMapping(ITicket ticket) {
+        OrderDtoOutput orderDtoOutput = OrderMapper.orderOutputMapping(ticket.getOrder());
+        return new TicketDtoOutPut(orderDtoOutput, ticket.getId(), ticket.getOrderId(), ticket.getCreateAt(),
+                ticket.getVersion());
+    }
+}
