1120b87cc8694f7e0ae60ad69a6dff8722d47fdd
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-11-05 12:36:58 +0100

    Mapper for Order get fix

--- src/main/java/groupId/artifactId/core/mapper/TicketMapper.java
+++ src/main/java/groupId/artifactId/core/mapper/TicketMapper.java
@@ -6,6 +6,10 @@ import groupId.artifactId.dao.entity.api.ITicket;
 
 public class TicketMapper {
     public static TicketDtoOutPut ticketOutputMapping(ITicket ticket) {
+        return new TicketDtoOutPut(new OrderDtoOutput(), ticket.getId(), ticket.getOrderId(), ticket.getCreateAt(),
+                ticket.getVersion());
+    }
+    public static TicketDtoOutPut ticketOutputForAllDataMapping(ITicket ticket) {
         OrderDtoOutput orderDtoOutput = OrderMapper.orderOutputMapping(ticket.getOrder());
         return new TicketDtoOutPut(orderDtoOutput, ticket.getId(), ticket.getOrderId(), ticket.getCreateAt(),
                 ticket.getVersion());
