b39e21747cbd3909a58a82fd1a1007987174cd23
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-11-11 11:01:36 +0100

    new Test for Order Validator

---
+++ src/test/java/groupId/artifactId/controller/validator/OrderValidatorTest.java
@@ -0,0 +1,47 @@
+package groupId.artifactId.controller.validator;
+
+import groupId.artifactId.core.dto.input.OrderDtoInput;
+import groupId.artifactId.core.dto.input.SelectedItemDtoInput;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+class OrderValidatorTest {
+    @InjectMocks
+    private OrderValidator orderValidator;
+
+    @Test
+    void validatePreconditionOne() {
+        // preconditions
+        final int count = 5;
+        final String messageExpected = "Menu item id in Order is not valid";
+        final OrderDtoInput orderDtoInput = new OrderDtoInput(Collections.singletonList(new SelectedItemDtoInput(null, count)));
+
+        //test
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> orderValidator.validate(orderDtoInput));
+
+        // assert
+        Assertions.assertEquals(messageExpected, exception.getMessage());
+    }
+
+    @Test
+    void validatePreconditionTwo() {
+        // preconditions
+        final long id = 1L;
+        final String messageExpected = "Menu item count in Order is not valid";
+        final OrderDtoInput orderDtoInput = new OrderDtoInput(Collections.singletonList(new SelectedItemDtoInput(id, null)));
+
+        //test
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> orderValidator.validate(orderDtoInput));
+
+        // assert
+        Assertions.assertEquals(messageExpected, exception.getMessage());
+    }
+}
