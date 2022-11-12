9bb7f2ee89bc48bc3e70ad091b890b1ec70c3a06
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-11-12 12:36:31 +0100

    MenuItem Mapper static methods was removed

--- src/main/java/groupId/artifactId/core/mapper/MenuItemMapper.java
+++ src/main/java/groupId/artifactId/core/mapper/MenuItemMapper.java
@@ -7,11 +7,14 @@ import groupId.artifactId.dao.entity.MenuItem;
 import groupId.artifactId.dao.entity.api.IMenuItem;
 
 public class MenuItemMapper {
-    public static IMenuItem menuItemInputMapping(MenuItemDtoInput menuItemDtoInput) {
+    public MenuItemMapper() {
+    }
+
+    public IMenuItem menuItemInputMapping(MenuItemDtoInput menuItemDtoInput) {
         return new MenuItem(menuItemDtoInput.getPrice(), menuItemDtoInput.getPizzaInfoId(), menuItemDtoInput.getMenuId());
     }
 
-    public static MenuItemDtoOutput menuItemOutputMapping(IMenuItem menuItem) {
+    public MenuItemDtoOutput menuItemOutputMapping(IMenuItem menuItem) {
         if (menuItem.getInfo()==null){
             return new MenuItemDtoOutput(menuItem.getId(), menuItem.getPrice(), menuItem.getPizzaInfoId(), menuItem.getCreationDate(),
                     menuItem.getVersion(), menuItem.getMenuId(), new PizzaInfoDtoOutput());
