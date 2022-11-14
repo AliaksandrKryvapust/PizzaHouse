813d2456654c9b189ae378153e102dc702d5f804
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-11-14 14:31:12 +0100

    Exceptions were changed at Menu Service

--- src/main/java/groupId/artifactId/service/MenuService.java
+++ src/main/java/groupId/artifactId/service/MenuService.java
@@ -6,6 +6,9 @@ import groupId.artifactId.core.dto.output.crud.MenuDtoCrudOutput;
 import groupId.artifactId.core.mapper.MenuMapper;
 import groupId.artifactId.dao.api.IMenuDao;
 import groupId.artifactId.dao.entity.api.IMenu;
+import groupId.artifactId.exceptions.DaoException;
+import groupId.artifactId.exceptions.OptimisticLockException;
+import groupId.artifactId.exceptions.ServiceException;
 import groupId.artifactId.service.api.IMenuService;
 
 import java.util.ArrayList;
@@ -22,49 +25,107 @@ public class MenuService implements IMenuService {
 
     @Override
     public List<MenuDtoCrudOutput> get() {
-        List<MenuDtoCrudOutput> temp = new ArrayList<>();
-        for (IMenu menu : this.dao.get()) {
-            MenuDtoCrudOutput menuDtoOutput = menuMapper.outputCrudMapping(menu);
-            temp.add(menuDtoOutput);
+        try {
+            List<MenuDtoCrudOutput> temp = new ArrayList<>();
+            for (IMenu menu : this.dao.get()) {
+                MenuDtoCrudOutput menuDtoOutput = menuMapper.outputCrudMapping(menu);
+                temp.add(menuDtoOutput);
+            }
+            return temp;
+        } catch (DaoException e) {
+            throw new ServiceException(e.getMessage(), e);
+        } catch (Exception e) {
+            throw new ServiceException("Failed to get List of Menu at Service", e);
         }
-        return temp;
     }
 
     @Override
     public MenuDtoCrudOutput get(Long id) {
-        return menuMapper.outputCrudMapping(this.dao.get(id));
+        try {
+            return menuMapper.outputCrudMapping(this.dao.get(id));
+        } catch (DaoException e) {
+            throw new ServiceException(e.getMessage(), e);
+        } catch (Exception e) {
+            throw new ServiceException("Failed to get Menu at Service by id" + id, e);
+        }
     }
 
     @Override
     public MenuDtoOutput getAllData(Long id) {
-        return menuMapper.outputMapping(this.dao.getAllData(id));
+        try {
+            return menuMapper.outputMapping(this.dao.getAllData(id));
+        } catch (DaoException e) {
+            throw new ServiceException(e.getMessage(), e);
+        } catch (Exception e) {
+            throw new ServiceException("Failed to getAll data from Menu at Service by id" + id, e);
+        }
     }
 
     @Override
     public Boolean isIdValid(Long id) {
-        return this.dao.exist(id);
+        try {
+            return this.dao.exist(id);
+        } catch (DaoException e) {
+            throw new ServiceException(e.getMessage(), e);
+        } catch (Exception e) {
+            throw new ServiceException("Failed to check Menu at Service by id " + id, e);
+        }
     }
 
     @Override
     public Boolean exist(String name) {
-        return this.dao.doesMenuExist(name);
+        try {
+            return this.dao.doesMenuExist(name);
+        } catch (DaoException e) {
+            throw new ServiceException(e.getMessage(), e);
+        } catch (Exception e) {
+            throw new ServiceException("Failed to check Menu at Service by name: " + name, e);
+        }
     }
 
     @Override
     public MenuDtoCrudOutput update(MenuDtoInput menuDtoInput, String id, String version) {
-        IMenu menu = this.dao.update(menuMapper.inputMapping(menuDtoInput), Long.valueOf(id), Integer.valueOf(version));
-        return menuMapper.outputCrudMapping(menu);
+        try {
+            IMenu menu = this.dao.update(menuMapper.inputMapping(menuDtoInput), Long.valueOf(id), Integer.valueOf(version));
+            return menuMapper.outputCrudMapping(menu);
+        } catch (DaoException e) {
+            throw new ServiceException(e.getMessage(), e);
+        } catch (IllegalStateException e) {
+            throw new IllegalStateException(e);
+        } catch (OptimisticLockException e) {
+            throw new OptimisticLockException(e.getMessage());
+        } catch (Exception e) {
+            throw new ServiceException("Failed to update Menu " + menuDtoInput + "by id:" + id, e);
+        }
     }
 
     @Override
     public void delete(String id, String version, String delete) {
-        this.dao.delete(Long.valueOf(id), Integer.valueOf(version), Boolean.valueOf(delete));
+        try {
+            this.dao.delete(Long.valueOf(id), Integer.valueOf(version), Boolean.valueOf(delete));
+        } catch (DaoException e) {
+            throw new ServiceException(e.getMessage(), e);
+        } catch (IllegalStateException e) {
+            throw new IllegalStateException(e);
+        } catch (OptimisticLockException e) {
+            throw new OptimisticLockException(e.getMessage());
+        } catch (Exception e) {
+            throw new ServiceException("Failed to delete Menu with id:" + id, e);
+        }
     }
 
     @Override
     public MenuDtoCrudOutput save(MenuDtoInput menuDtoInput) {
-        IMenu menu = this.dao.save(menuMapper.inputMapping(menuDtoInput));
-        return menuMapper.outputCrudMapping(menu);
+        try {
+            IMenu menu = this.dao.save(menuMapper.inputMapping(menuDtoInput));
+            return menuMapper.outputCrudMapping(menu);
+        } catch (DaoException e) {
+            throw new ServiceException(e.getMessage(), e);
+        } catch (IllegalStateException e) {
+            throw new IllegalStateException(e);
+        } catch (Exception e) {
+            throw new ServiceException("Failed to save Menu item" + menuDtoInput, e);
+        }
     }
 
 }
