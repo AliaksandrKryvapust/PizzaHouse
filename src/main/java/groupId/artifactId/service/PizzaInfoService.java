3b32f82ad85d6b0846a3e124649b5d12b426c18a
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-11-14 14:47:53 +0100

    Exceptions were changed at Pizza Info Service

--- src/main/java/groupId/artifactId/service/PizzaInfoService.java
+++ src/main/java/groupId/artifactId/service/PizzaInfoService.java
@@ -5,6 +5,9 @@ import groupId.artifactId.core.dto.output.PizzaInfoDtoOutput;
 import groupId.artifactId.core.mapper.PizzaInfoMapper;
 import groupId.artifactId.dao.api.IPizzaInfoDao;
 import groupId.artifactId.dao.entity.api.IPizzaInfo;
+import groupId.artifactId.exceptions.DaoException;
+import groupId.artifactId.exceptions.OptimisticLockException;
+import groupId.artifactId.exceptions.ServiceException;
 import groupId.artifactId.service.api.IPizzaInfoService;
 
 import java.util.ArrayList;
@@ -23,44 +26,96 @@ public class PizzaInfoService implements IPizzaInfoService {
 
     @Override
     public PizzaInfoDtoOutput save(PizzaInfoDtoInput pizzaInfoDtoInput) {
-        IPizzaInfo pizzaInfo = this.dao.save(pizzaInfoMapper.inputMapping(pizzaInfoDtoInput));
-        return pizzaInfoMapper.outputMapping(pizzaInfo);
+        try {
+            IPizzaInfo pizzaInfo = this.dao.save(pizzaInfoMapper.inputMapping(pizzaInfoDtoInput));
+            return pizzaInfoMapper.outputMapping(pizzaInfo);
+        } catch (DaoException e) {
+            throw new ServiceException(e.getMessage(), e);
+        } catch (IllegalStateException e) {
+            throw new IllegalStateException(e);
+        } catch (Exception e) {
+            throw new ServiceException("Failed to save Pizza Info" + pizzaInfoDtoInput, e);
+        }
     }
 
     @Override
     public List<PizzaInfoDtoOutput> get() {
-        List<PizzaInfoDtoOutput> temp = new ArrayList<>();
-        for (IPizzaInfo pizzaInfo : this.dao.get()) {
-            PizzaInfoDtoOutput pizzaInfoDtoOutput = pizzaInfoMapper.outputMapping(pizzaInfo);
-            temp.add(pizzaInfoDtoOutput);
+        try {
+            List<PizzaInfoDtoOutput> temp = new ArrayList<>();
+            for (IPizzaInfo pizzaInfo : this.dao.get()) {
+                PizzaInfoDtoOutput pizzaInfoDtoOutput = pizzaInfoMapper.outputMapping(pizzaInfo);
+                temp.add(pizzaInfoDtoOutput);
+            }
+            return temp;
+        } catch (DaoException e) {
+            throw new ServiceException(e.getMessage(), e);
+        } catch (Exception e) {
+            throw new ServiceException("Failed to get List of Pizza Info`s at Service", e);
         }
-        return temp;
     }
 
     @Override
     public PizzaInfoDtoOutput get(Long id) {
-        return pizzaInfoMapper.outputMapping(this.dao.get(id));
+        try {
+            return pizzaInfoMapper.outputMapping(this.dao.get(id));
+        } catch (DaoException e) {
+            throw new ServiceException(e.getMessage(), e);
+        } catch (Exception e) {
+            throw new ServiceException("Failed to get Pizza Info at Service by id" + id, e);
+        }
     }
 
     @Override
     public Boolean isIdValid(Long id) {
-        return this.dao.exist(id);
+        try {
+            return this.dao.exist(id);
+        } catch (DaoException e) {
+            throw new ServiceException(e.getMessage(), e);
+        } catch (Exception e) {
+            throw new ServiceException("Failed to check Pizza Info at Service by id " + id, e);
+        }
     }
 
     @Override
     public Boolean exist(String name) {
-        return this.dao.doesPizzaExist(name);
+        try {
+            return this.dao.doesPizzaExist(name);
+        } catch (DaoException e) {
+            throw new ServiceException(e.getMessage(), e);
+        } catch (Exception e) {
+            throw new ServiceException("Failed to check Pizza Info at Service by name " + name, e);
+        }
     }
 
     @Override
     public PizzaInfoDtoOutput update(PizzaInfoDtoInput pizzaInfoDtoInput, String id, String version) {
-        IPizzaInfo pizzaInfo = this.dao.update(pizzaInfoMapper.inputMapping(pizzaInfoDtoInput),
-                Long.valueOf(id), Integer.valueOf(version));
-        return pizzaInfoMapper.outputMapping(pizzaInfo);
+        try {
+            IPizzaInfo pizzaInfo = this.dao.update(pizzaInfoMapper.inputMapping(pizzaInfoDtoInput),
+                    Long.valueOf(id), Integer.valueOf(version));
+            return pizzaInfoMapper.outputMapping(pizzaInfo);
+        } catch (DaoException e) {
+            throw new ServiceException(e.getMessage(), e);
+        } catch (IllegalStateException e) {
+            throw new IllegalStateException(e);
+        } catch (OptimisticLockException e) {
+            throw new OptimisticLockException(e.getMessage());
+        } catch (Exception e) {
+            throw new ServiceException("Failed to update Pizza Info " + pizzaInfoDtoInput + "by id:" + id, e);
+        }
     }
 
     @Override
     public void delete(String id, String version, String delete) {
-        this.dao.delete(Long.valueOf(id), Integer.valueOf(version), Boolean.valueOf(delete));
+        try {
+            this.dao.delete(Long.valueOf(id), Integer.valueOf(version), Boolean.valueOf(delete));
+        } catch (DaoException e) {
+            throw new ServiceException(e.getMessage(), e);
+        } catch (IllegalStateException e) {
+            throw new IllegalStateException(e);
+        } catch (OptimisticLockException e) {
+            throw new OptimisticLockException(e.getMessage());
+        } catch (Exception e) {
+            throw new ServiceException("Failed to delete Pizza Info with id:" + id, e);
+        }
     }
 }
