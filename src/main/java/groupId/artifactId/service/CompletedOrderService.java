58bb8f6e992c6ab89cb3b8f39dfc0ce69887100f
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-11-14 14:16:33 +0100

    Exceptions were changed at Completed order Service

--- src/main/java/groupId/artifactId/service/CompletedOrderService.java
+++ src/main/java/groupId/artifactId/service/CompletedOrderService.java
@@ -9,6 +9,8 @@ import groupId.artifactId.dao.entity.CompletedOrder;
 import groupId.artifactId.dao.entity.Pizza;
 import groupId.artifactId.dao.entity.api.ICompletedOrder;
 import groupId.artifactId.dao.entity.api.IPizza;
+import groupId.artifactId.exceptions.DaoException;
+import groupId.artifactId.exceptions.ServiceException;
 import groupId.artifactId.service.api.ICompletedOrderService;
 
 import java.util.ArrayList;
@@ -20,6 +22,7 @@ public class CompletedOrderService implements ICompletedOrderService {
 
     private final CompletedOrderMapper completedOrderMapper;
 
+
     public CompletedOrderService(ICompletedOrderDao completedOrderDao, IPizzaDao pizzaDao, CompletedOrderMapper completedOrderMapper) {
         this.completedOrderDao = completedOrderDao;
         this.pizzaDao = pizzaDao;
@@ -28,48 +31,92 @@ public class CompletedOrderService implements ICompletedOrderService {
 
     @Override
     public CompletedOrderDtoOutput getAllData(Long id) {
-        return completedOrderMapper.outputMapping(this.completedOrderDao.getAllData(id));
+        try {
+            return completedOrderMapper.outputMapping(this.completedOrderDao.getAllData(id));
+        } catch (DaoException e) {
+            throw new ServiceException(e.getMessage(), e);
+        } catch (Exception e) {
+            throw new ServiceException("Failed to getAllData at Completed order Service by id" + id, e);
+        }
     }
 
     @Override
     public ICompletedOrder getAllDataRow(Long id) {
-        return this.completedOrderDao.getAllData(id);
+        try {
+            return this.completedOrderDao.getAllData(id);
+        } catch (DaoException e) {
+            throw new ServiceException(e.getMessage(), e);
+        } catch (Exception e) {
+            throw new ServiceException("Failed to getAllDataRow at Completed order Service by id" + id, e);
+        }
     }
 
     @Override
     public Boolean isOrderIdValid(Long id) {
-        return this.completedOrderDao.exist(id);
+        try {
+            return this.completedOrderDao.exist(id);
+        } catch (DaoException e) {
+            throw new ServiceException(e.getMessage(), e);
+        } catch (Exception e) {
+            throw new ServiceException("Failed to check Order state at Completed order Service by id" + id, e);
+        }
     }
 
     @Override
     public Boolean isPizzaIdValid(Long id) {
-        return this.pizzaDao.exist(id);
+        try {
+            return this.pizzaDao.exist(id);
+        } catch (DaoException e) {
+            throw new ServiceException(e.getMessage(), e);
+        } catch (Exception e) {
+            throw new ServiceException("Failed to check pizza state at Completed order Service by id" + id, e);
+        }
     }
 
     @Override
     public CompletedOrderDtoCrudOutput save(ICompletedOrder type) {
-        ICompletedOrder completedOrder = this.completedOrderDao.save(type);
-        List<IPizza> pizzas = new ArrayList<>();// completedOrderId needed
-        for (IPizza pizza : type.getItems()) {
-            IPizza output = this.pizzaDao.save(new Pizza(completedOrder.getId(), pizza.getName(), pizza.getSize()));
-            pizzas.add(output);
+        try {
+            ICompletedOrder completedOrder = this.completedOrderDao.save(type);
+            List<IPizza> pizzas = new ArrayList<>();// completedOrderId needed
+            for (IPizza pizza : type.getItems()) {
+                IPizza output = this.pizzaDao.save(new Pizza(completedOrder.getId(), pizza.getName(), pizza.getSize()));
+                pizzas.add(output);
+            }
+            return completedOrderMapper.outputCrudMapping(new CompletedOrder(completedOrder.getTicket(), pizzas,
+                    completedOrder.getId(), completedOrder.getTicketId()));
+        } catch (DaoException e) {
+            throw new ServiceException(e.getMessage(), e);
+        } catch (IllegalStateException e) {
+            throw new IllegalStateException(e);
+        } catch (Exception e) {
+            throw new ServiceException("Failed to save Completed order" + type, e);
         }
-        return completedOrderMapper.outputCrudMapping(new CompletedOrder(completedOrder.getTicket(), pizzas,
-                completedOrder.getId(), completedOrder.getTicketId()));
     }
 
     @Override
     public List<CompletedOrderDtoCrudOutput> get() {
-        List<CompletedOrderDtoCrudOutput> outputs = new ArrayList<>();
-        for (ICompletedOrder order : this.completedOrderDao.get()) {
-            CompletedOrderDtoCrudOutput output = completedOrderMapper.outputCrudMapping(order);
-            outputs.add(output);
+        try {
+            List<CompletedOrderDtoCrudOutput> outputs = new ArrayList<>();
+            for (ICompletedOrder order : this.completedOrderDao.get()) {
+                CompletedOrderDtoCrudOutput output = completedOrderMapper.outputCrudMapping(order);
+                outputs.add(output);
+            }
+            return outputs;
+        } catch (DaoException e) {
+            throw new ServiceException(e.getMessage(), e);
+        } catch (Exception e) {
+            throw new ServiceException("Failed to get List of Completed order at Service", e);
         }
-        return outputs;
     }
 
     @Override
     public CompletedOrderDtoCrudOutput get(Long id) {
-        return completedOrderMapper.outputCrudMapping(this.completedOrderDao.get(id));
+        try {
+            return completedOrderMapper.outputCrudMapping(this.completedOrderDao.get(id));
+        } catch (DaoException e) {
+            throw new ServiceException(e.getMessage(), e);
+        } catch (Exception e) {
+            throw new ServiceException("Failed to getAllData at Completed order Service by id" + id, e);
+        }
     }
 }
