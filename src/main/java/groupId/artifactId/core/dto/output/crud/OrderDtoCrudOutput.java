d6d24eab48371a11d2232d7908ac09bf670fe39f
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-11-16 10:52:51 +0100

    Order Dto Crud lombok

--- src/main/java/groupId/artifactId/core/dto/output/crud/OrderDtoCrudOutput.java
+++ src/main/java/groupId/artifactId/core/dto/output/crud/OrderDtoCrudOutput.java
@@ -1,46 +1,15 @@
 package groupId.artifactId.core.dto.output.crud;
 
+import lombok.*;
+
 import java.time.Instant;
 
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Getter
+@ToString
 public class OrderDtoCrudOutput {
-    private Long id;
+    private final @NonNull Long id;
     private Instant createdAt;
     private Integer version;
-
-    public OrderDtoCrudOutput() {
-    }
-
-    public OrderDtoCrudOutput(Long id) {
-        this.id = id;
-    }
-
-    public OrderDtoCrudOutput(Long id, Instant createdAt, Integer version) {
-        this.id = id;
-        this.createdAt = createdAt;
-        this.version = version;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Instant getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Instant createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Integer getVersion() {
-        return version;
-    }
-
-    public void setVersion(Integer version) {
-        this.version = version;
-    }
 }
