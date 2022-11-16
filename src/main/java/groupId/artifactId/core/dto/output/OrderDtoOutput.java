3c9cede6f3ddbd086836434163ce955bfbca8b53
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-11-16 16:47:57 +0100

    Order Dto lombok

--- src/main/java/groupId/artifactId/core/dto/output/OrderDtoOutput.java
+++ src/main/java/groupId/artifactId/core/dto/output/OrderDtoOutput.java
@@ -1,53 +1,16 @@
 package groupId.artifactId.core.dto.output;
 
+import lombok.*;
+
 import java.time.Instant;
 import java.util.List;
 
+@AllArgsConstructor
+@Getter
+@ToString
 public class OrderDtoOutput {
-    private List<SelectedItemDtoOutput> selectedItems;
-    private Long id;
-    private Instant createdAt;
-    private Integer version;
-
-    public OrderDtoOutput() {
-    }
-
-    public OrderDtoOutput(List<SelectedItemDtoOutput> selectedItems, Long id, Instant createdAt, Integer version) {
-        this.selectedItems = selectedItems;
-        this.id = id;
-        this.createdAt = createdAt;
-        this.version = version;
-    }
-
-    public List<SelectedItemDtoOutput> getSelectedItems() {
-        return selectedItems;
-    }
-
-    public void setSelectedItems(List<SelectedItemDtoOutput> selectedItems) {
-        this.selectedItems = selectedItems;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Instant getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Instant createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Integer getVersion() {
-        return version;
-    }
-
-    public void setVersion(Integer version) {
-        this.version = version;
-    }
+    private final @NonNull List<SelectedItemDtoOutput> selectedItems;
+    private final @NonNull Long id;
+    private final @NonNull Instant createdAt;
+    private final @NonNull Integer version;
 }
