5afbdb5c6681d1c6d0c7d3fa77c59c487e1d8bae
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-11-17 11:56:37 +0100

    New exception

---
+++ src/main/java/groupId/artifactId/exceptions/NoContentException.java
@@ -0,0 +1,7 @@
+package groupId.artifactId.exceptions;
+
+public class NoContentException extends RuntimeException {
+    public NoContentException(String errorMsg) {
+        super(errorMsg);
+    }
+}
