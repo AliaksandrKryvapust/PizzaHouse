3fcc7160da45529a78e2822d90e4300fc9a86522
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-11-20 19:33:46 +0100

    jackson millis serialisation/des

--- src/main/java/groupId/artifactId/controller/utils/api/JsonCreator.java
+++ src/main/java/groupId/artifactId/controller/utils/api/JsonCreator.java
@@ -1,8 +1,11 @@
 package groupId.artifactId.controller.utils.api;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.beans.PropertyVetoException;
@@ -13,8 +16,13 @@ public class JsonCreator {
 
     private JsonCreator() {
         this.mapper = new ObjectMapper(new JsonFactory());
-        mapper.registerModule(new JavaTimeModule());
-        mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+        mapper.registerModule(new JavaTimeModule())
+                .configure(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS, false)
+                .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, true)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true)
+                .configure(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS, false)
+                .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+                .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
     }
 
     public static ObjectMapper getInstance() throws PropertyVetoException {
