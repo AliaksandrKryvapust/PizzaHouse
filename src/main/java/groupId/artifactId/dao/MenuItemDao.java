61a09a9e193697b5f0a670938f73381d4e746386
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-11-22 18:57:18 +0100

    Hibernate Service doGet for Menu Item cascade

--- src/main/java/groupId/artifactId/dao/MenuItemDao.java
+++ src/main/java/groupId/artifactId/dao/MenuItemDao.java
@@ -2,38 +2,24 @@ package groupId.artifactId.dao;
 
 import groupId.artifactId.dao.api.IMenuItemDao;
 import groupId.artifactId.dao.entity.MenuItem;
-import groupId.artifactId.dao.entity.PizzaInfo;
 import groupId.artifactId.dao.entity.api.IMenuItem;
-import groupId.artifactId.dao.entity.api.IPizzaInfo;
 import groupId.artifactId.exceptions.DaoException;
 import groupId.artifactId.exceptions.NoContentException;
-import groupId.artifactId.exceptions.OptimisticLockException;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.OptimisticLockException;
 
-import javax.sql.DataSource;
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static groupId.artifactId.core.Constants.*;
 
 public class MenuItemDao implements IMenuItemDao {
-    private final DataSource dataSource;
-    private static final String INSERT_MENU_ITEM_SQL = "INSERT INTO pizza_manager.menu_item (price, pizza_info_id, menu_id)" +
-            " VALUES (?, ?, ?);";
-    private static final String UPDATE_MENU_ITEM_SQL = "UPDATE pizza_manager.menu_item SET price=?, pizza_info_id=?, " +
-            "menu_id=?, version=version+1 WHERE id=? AND version=?;";
-    private static final String SELECT_MENU_ITEM_SQL = "SELECT id, price, pizza_info_id, creation_date, version, menu_id " +
-            "FROM pizza_manager.menu_item ORDER BY id;";
-    private static final String SELECT_MENU_ITEM_BY_ID_SQL = "SELECT id, price, pizza_info_id, creation_date, version," +
-            " menu_id FROM pizza_manager.menu_item WHERE id=?;";
-    private static final String SELECT_MENU_ITEM_ALL_DATA_SQL = "SELECT menu_item.id AS id, price, pizza_info_id, " +
-            "menu_item.creation_date AS cd, menu_item.version AS ver, menu_id, name, description, size," +
-            "pi.creation_date AS picd, pi.version AS piv, menu_id FROM pizza_manager.menu_item " +
-            "INNER JOIN pizza_manager.pizza_info pi on menu_item.pizza_info_id = pi.id WHERE menu_item.id=?;";
-    private static final String DELETE_MENU_ITEM_SQL = "DELETE FROM pizza_manager.menu_item WHERE id=?;";
+    private static final String SELECT_MENU_ITEM = "SELECT menuItem from MenuItem menuItem ORDER BY id";
+    private final EntityManager entityManager;
 
-    public MenuItemDao(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public MenuItemDao(EntityManager entityManager) {
+        this.entityManager = entityManager;
     }
 
     @Override
@@ -41,29 +27,16 @@ public class MenuItemDao implements IMenuItemDao {
         if (menuItem.getId() != null || menuItem.getVersion() != null) {
             throw new IllegalStateException("MenuItem id & version should be empty");
         }
-        try (Connection con = dataSource.getConnection()) {
-            try (PreparedStatement statement = con.prepareStatement(INSERT_MENU_ITEM_SQL, Statement.RETURN_GENERATED_KEYS)) {
-                long rows = 0;
-                statement.setDouble(1, menuItem.getPrice());
-                statement.setLong(2, menuItem.getPizzaInfoId());
-                statement.setLong(3, menuItem.getMenuId());
-                rows += statement.executeUpdate();
-                if (rows > 1) {
-                    throw new IllegalStateException("Incorrect menu_item table update, more than 1 row affected");
-                }
-                try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
-                    generatedKeys.next();
-                    return new MenuItem(generatedKeys.getLong(1), menuItem.getPrice(),
-                            menuItem.getPizzaInfoId(), menuItem.getMenuId());
-                }
-            }
-        } catch (SQLException e) {
-            if (e.getMessage().contains(MENU_ITEM_FK) || e.getMessage().contains(MENU_ITEM_FK2)
-                    || e.getMessage().contains(MENU_ITEM_UK)) {
-                throw new NoContentException("menu_item table insert failed, check preconditions and FK values: "
-                + menuItem);
+        try {
+            entityManager.persist(menuItem);
+            return menuItem;
+        } catch (Exception e) {
+            if (e.getMessage().contains(MENU_ITEM_UK) || e.getMessage().contains(MENU_ITEM_FK)
+                    || e.getMessage().contains(MENU_ITEM_FK2)) {
+                throw new NoContentException("menu item table insert failed,  check preconditions and FK values: "
+                        + menuItem);
             } else {
-                throw new DaoException("Failed to save new MenuItem" + menuItem, e);
+                throw new DaoException("Failed to save new MenuItem" + menuItem + "\t cause" + e.getMessage(), e);
             }
         }
     }
@@ -73,128 +46,89 @@ public class MenuItemDao implements IMenuItemDao {
         if (menuItem.getId() != null || menuItem.getVersion() != null) {
             throw new IllegalStateException("MenuItem id & version should be empty");
         }
-        try (Connection con = dataSource.getConnection()) {
-            try (PreparedStatement statement = con.prepareStatement(UPDATE_MENU_ITEM_SQL)) {
-                long rows = 0;
-                statement.setDouble(1, menuItem.getPrice());
-                statement.setLong(2, menuItem.getPizzaInfoId());
-                statement.setLong(3, menuItem.getMenuId());
-                statement.setLong(4, id);
-                statement.setInt(5, version);
-                rows += statement.executeUpdate();
-                if (rows == 0) {
-                    throw new OptimisticLockException("menu_item table update failed, version does not match update denied");
-                }
-                if (rows > 1) {
-                    throw new IllegalStateException("Incorrect menu_item table update, more than 1 row affected");
-                }
-                return new MenuItem(id, menuItem.getPrice(), menuItem.getPizzaInfoId(), menuItem.getMenuId());
+        try {
+            MenuItem currentEntity = (MenuItem) this.getLock(id);
+            entityManager.detach(currentEntity);
+            if (!currentEntity.getVersion().equals(version)) {
+                throw new OptimisticLockException();
             }
-        } catch (SQLException e) {
-            throw new DaoException("Failed to update menu_item" + menuItem + " by id:" + id, e);
+            currentEntity.setPrice(menuItem.getPrice());
+//            currentEntity.setPizzaInfoId(menuItem.getPizzaInfoId());
+            currentEntity.setMenuId(menuItem.getMenuId());
+            entityManager.merge(currentEntity);
+            return currentEntity;
+        } catch (NoContentException e) {
+            throw new NoContentException(e.getMessage());
+        } catch (OptimisticLockException e) {
+            throw new OptimisticLockException("menu_item table update failed, version does not match update denied");
+        } catch (Exception e) {
+            if (e.getMessage().contains(MENU_ITEM_UK) || e.getMessage().contains(MENU_ITEM_FK)
+                    || e.getMessage().contains(MENU_ITEM_FK2)) {
+                throw new NoContentException("menu_item table insert failed,  check preconditions and FK values: "
+                        + menuItem);
+            } else {
+                throw new DaoException("Failed to update menu_item" + menuItem + " by id:" + id + "\t cause" + e.getMessage(), e);
+            }
         }
-    }
-
-    @Override
-    public IMenuItem getLock(Long id) {
-        return null;
     }
 
     @Override
     public IMenuItem get(Long id) {
-        try (Connection con = dataSource.getConnection()) {
-            try (PreparedStatement statement = con.prepareStatement(SELECT_MENU_ITEM_BY_ID_SQL)) {
-                statement.setLong(1, id);
-                try (ResultSet resultSet = statement.executeQuery()) {
-                    resultSet.next();
-                    if (!resultSet.isLast()) {
-                        throw new NoContentException("There is no Menu Item with id:" + id);
-                    }
-                    return this.mapper(resultSet);
-                }
+        try {
+            MenuItem menuItem = entityManager.find(MenuItem.class, id);
+            if (menuItem == null) {
+                throw new NoContentException("There is no Menu Item with id:" + id);
             }
-        } catch (SQLException e) {
-            throw new DaoException("Failed to get Menu Item by id:" + id, e);
+            return menuItem;
+        } catch (NoContentException e) {
+            throw new NoContentException(e.getMessage());
+        } catch (Exception e) {
+            throw new DaoException("Failed to get Menu Item from Data Base by id:" + id + "cause: " + e.getMessage(), e);
         }
     }
 
     @Override
-    public void delete(Long id, Boolean delete){
-        try (Connection con = dataSource.getConnection()) {
-            try (PreparedStatement statement = con.prepareStatement(DELETE_MENU_ITEM_SQL)) {
-                long rows = 0;
-                statement.setLong(1, id);
-                rows += statement.executeUpdate();
-                if (rows > 1) {
-                    throw new IllegalStateException("Incorrect pizza_info table delete, more than 1 row affected");
-                }
+    public void delete(Long id, Boolean delete) {
+        try {
+            MenuItem menuItem = entityManager.find(MenuItem.class, id);
+            if (menuItem == null) {
+                throw new NoContentException("There is no Menu Item with id:" + id);
             }
-        } catch (SQLException e) {
-            throw new DaoException("Failed to delete menu item with id:" + id, e);
+            entityManager.remove(menuItem);
+        } catch (NoContentException e) {
+            throw new NoContentException(e.getMessage());
+        } catch (Exception e) {
+            throw new DaoException("Failed to delete Menu Item with id:" + id + "\tcause: " + e.getMessage(), e);
         }
     }
 
     @Override
     public List<IMenuItem> get() {
-        try (Connection con = dataSource.getConnection()) {
-            List<IMenuItem> items = new ArrayList<>();
-            try (PreparedStatement statement = con.prepareStatement(SELECT_MENU_ITEM_SQL)) {
-                try (ResultSet resultSet = statement.executeQuery()) {
-                    while (resultSet.next()) {
-                        items.add(this.mapper(resultSet));
-                    }
-                }
-                return items;
+        try {
+            List<?> iMenuItems = entityManager.createQuery(SELECT_MENU_ITEM).getResultList();
+            List<IMenuItem> output = iMenuItems.stream().filter((i) -> i instanceof IMenuItem)
+                    .map(IMenuItem.class::cast).collect(Collectors.toList());
+            if (!output.isEmpty()) {
+                return output;
+            } else {
+                throw new IllegalStateException("Failed to get List of menu Item");
             }
         } catch (Exception e) {
-            throw new DaoException("Failed to get List of menu items", e);
+            throw new DaoException("Failed to get List of menu Item\tcause: " + e.getMessage(), e);
         }
     }
 
-    @Override
-    public IMenuItem getAllData(Long id) {
-        try (Connection con = dataSource.getConnection()) {
-            try (PreparedStatement statement = con.prepareStatement(SELECT_MENU_ITEM_ALL_DATA_SQL)) {
-                statement.setLong(1, id);
-                try (ResultSet resultSet = statement.executeQuery()) {
-                    resultSet.next();
-                    return this.allDataMapper(resultSet);
-                }
+    private IMenuItem getLock(Long id) {
+        try {
+            MenuItem menuItem = entityManager.find(MenuItem.class, id, LockModeType.OPTIMISTIC_FORCE_INCREMENT);
+            if (menuItem == null) {
+                throw new NoContentException("There is no Menu Item with id:" + id);
             }
-        } catch (SQLException e) {
-            throw new DaoException("Failed to get Menu Item with pizza info by id:" + id, e);
-        }
-    }
-
-    @Override
-    public Boolean exist(Long id) {
-        try (Connection con = dataSource.getConnection()) {
-            try (PreparedStatement statement = con.prepareStatement(SELECT_MENU_ITEM_BY_ID_SQL)) {
-                statement.setLong(1, id);
-                try (ResultSet resultSet = statement.executeQuery()) {
-                    return resultSet.next();
-                }
-            }
+            return menuItem;
+        } catch (NoContentException e) {
+            throw new NoContentException(e.getMessage());
         } catch (Exception e) {
-            throw new DaoException("Failed to select Menu Item with id:" + id, e);
+            throw new DaoException("Failed to get Lock of Menu Item from Data Base by id:" + id + "cause: " + e.getMessage(), e);
         }
-    }
-
-    private IMenuItem mapper(ResultSet resultSet) throws SQLException {
-        return new MenuItem(resultSet.getLong("id"), new PizzaInfo(), resultSet.getDouble("price"),
-                resultSet.getLong("pizza_info_id"), resultSet.getTimestamp("creation_date").toInstant(),
-                resultSet.getInt("version"), resultSet.getLong("menu_id"));
-    }
-    private IMenuItem allDataMapper(ResultSet resultSet) throws SQLException {
-        IPizzaInfo pizzaInfo =  new PizzaInfo(resultSet.getLong("pizza_info_id"), resultSet.getString("name"),
-                resultSet.getString("description"), resultSet.getInt("size"),
-                resultSet.getTimestamp("picd").toInstant(), resultSet.getInt("piv"));
-        MenuItem menuItem = new MenuItem(resultSet.getLong("id"), pizzaInfo, resultSet.getDouble("price"),
-                resultSet.getLong("pizza_info_id"), resultSet.getTimestamp("cd").toInstant(),
-                resultSet.getInt("ver"), resultSet.getLong("menu_id"));
-        if (menuItem.getInfo()==null || menuItem.getId()==null){
-            throw new NoContentException("There is no Menu Item with such id");
-        }
-        return menuItem;
     }
 }
--- src/main/java/groupId/artifactId/service/api/IMenuItemService.java
+++ src/main/java/groupId/artifactId/service/api/IMenuItemService.java
@@ -4,9 +4,6 @@ import groupId.artifactId.core.dto.input.MenuItemDtoInput;
 import groupId.artifactId.core.dto.output.MenuItemDtoOutput;
 import groupId.artifactId.core.dto.output.crud.MenuItemDtoCrudOutput;
 
-public interface IMenuItemService extends IService<MenuItemDtoCrudOutput, MenuItemDtoInput>,
+public interface IMenuItemService extends IService<MenuItemDtoOutput, MenuItemDtoInput>,
         IServiceUpdate<MenuItemDtoCrudOutput, MenuItemDtoInput>, IServiceDelete {
-    MenuItemDtoOutput getAllData(Long id);
-
-    Boolean isIdValid(Long id);
 }
