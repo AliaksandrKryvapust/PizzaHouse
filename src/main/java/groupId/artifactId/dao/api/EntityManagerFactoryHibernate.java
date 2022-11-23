86675a04d44600f9fe46152a14fdb7718b485082
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-11-23 17:11:45 +0100

    jakarta have been changed to javax

--- src/main/java/groupId/artifactId/dao/api/EntityManagerFactoryHibernate.java
+++ src/main/java/groupId/artifactId/dao/api/EntityManagerFactoryHibernate.java
@@ -1,8 +1,8 @@
 package groupId.artifactId.dao.api;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class EntityManagerFactoryHibernate {
     private static final EntityManagerFactory entityManagerFactory;
