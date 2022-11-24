af4453313fe7097a977389f25a1236393d006b9c
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-11-24 22:09:39 +0100

    Menu Item Dto input was changed for Hibernate

--- src/main/java/groupId/artifactId/core/dto/input/MenuItemDtoInput.java
+++ src/main/java/groupId/artifactId/core/dto/input/MenuItemDtoInput.java
@@ -12,6 +12,5 @@ import lombok.extern.jackson.Jacksonized;
 @Jacksonized
 public class MenuItemDtoInput {
     private final @NonNull Double price;
-    private final @NonNull Long menuId;
     private final @NonNull PizzaInfoDtoInput pizzaInfoDtoInput;
 }
