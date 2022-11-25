a43d8cd91c2b590d398948461e7e96cdf7497534
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-11-25 17:40:25 +0100

    Menu Service Test new method for MenuItem update

--- src/test/java/groupId/artifactId/service/MenuServiceTest.java
+++ src/test/java/groupId/artifactId/service/MenuServiceTest.java
@@ -101,6 +101,30 @@ class MenuServiceTest {
     }
 
     @Test
+    void getRow() {
+        // preconditions
+        final String name = "Optional Menu";
+        final boolean enable = false;
+        final long id = 1L;
+        final int version = 1;
+        final Instant creationDate = Instant.now();
+        final IMenu menu = Menu.builder().id(id).creationDate(creationDate).version(version).name(name)
+                .enable(enable).build();
+        Mockito.when(menuDao.getLock(any(Long.class), any(EntityManager.class))).thenReturn(menu);
+
+        //test
+        IMenu test = menuService.getRow(id, entityManager);
+
+        // assert
+        Assertions.assertNotNull(test);
+        Assertions.assertEquals(id, test.getId());
+        Assertions.assertEquals(name, test.getName());
+        Assertions.assertEquals(enable, test.getEnable());
+        Assertions.assertEquals(version, test.getVersion());
+        Assertions.assertEquals(creationDate, test.getCreationDate());
+    }
+
+    @Test
     void getAllData() {
         // preconditions
         final String name = "Optional Menu";
@@ -186,6 +210,58 @@ class MenuServiceTest {
         Assertions.assertEquals(enable, test.getEnable());
         Assertions.assertEquals(version, test.getVersion());
         Assertions.assertEquals(creationDate, test.getCreatedAt());
+    }
+
+
+    @Test
+    void updateItem() {
+        // preconditions
+        final String name = "Optional Menu";
+        final boolean enable = false;
+        final String pizzaName = "ITALIANO PIZZA";
+        final long id = 1L;
+        final String description = "Mozzarella cheese, basilica, ham";
+        final int size = 32;
+        final double price = 18.0;
+        final int version = 1;
+        final Instant creationDate = Instant.now();
+        final PizzaInfo pizzaInfo = PizzaInfo.builder().id(id).name(pizzaName).description(description).size(size)
+                .creationDate(creationDate).version(version).build();
+        final List<IMenuItem> items = singletonList(MenuItem.builder().id(id).pizzaInfo(pizzaInfo).price(price)
+                .creationDate(creationDate).version(version).build());
+        final IMenu menu = Menu.builder().id(id).creationDate(creationDate).version(version).name(name)
+                .enable(enable).items(items).build();
+        final IMenuItem item = MenuItem.builder().pizzaInfo(pizzaInfo).price(price).build();
+        Mockito.when(entityManager.getTransaction()).thenReturn(transaction);
+
+        Mockito.when(menuDao.updateItems(any(IMenu.class), any(IMenuItem.class), any(EntityManager.class)))
+                .thenReturn(menu);
+
+        //test
+        IMenu test = menuService.updateItem(menu, item, entityManager);
+
+        // assert
+        Assertions.assertNotNull(test);
+        Assertions.assertNotNull(test.getItems());
+        Assertions.assertEquals(id, test.getId());
+        Assertions.assertEquals(name, test.getName());
+        Assertions.assertEquals(enable, test.getEnable());
+        Assertions.assertEquals(version, test.getVersion());
+        Assertions.assertEquals(creationDate, test.getCreationDate());
+        for (IMenuItem output : test.getItems()) {
+            Assertions.assertNotNull(output);
+            Assertions.assertNotNull(output.getPizzaInfo());
+            Assertions.assertEquals(id, output.getId());
+            Assertions.assertEquals(price, output.getPrice());
+            Assertions.assertEquals(version, output.getVersion());
+            Assertions.assertEquals(creationDate, output.getCreationDate());
+            Assertions.assertEquals(id, output.getPizzaInfo().getId());
+            Assertions.assertEquals(pizzaName, output.getPizzaInfo().getName());
+            Assertions.assertEquals(description, output.getPizzaInfo().getDescription());
+            Assertions.assertEquals(size, output.getPizzaInfo().getSize());
+            Assertions.assertEquals(version, output.getPizzaInfo().getVersion());
+            Assertions.assertEquals(creationDate, output.getPizzaInfo().getCreationDate());
+        }
     }
 
     @Test
