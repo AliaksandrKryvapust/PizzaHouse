b938fe49542a9838969051db241ef22dcca655bc
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-11-25 17:39:43 +0100

    Menu Dao new method for MenuItem update

--- src/main/java/groupId/artifactId/dao/MenuDao.java
+++ src/main/java/groupId/artifactId/dao/MenuDao.java
@@ -3,11 +3,13 @@ package groupId.artifactId.dao;
 import groupId.artifactId.dao.api.IMenuDao;
 import groupId.artifactId.dao.entity.Menu;
 import groupId.artifactId.dao.entity.api.IMenu;
+import groupId.artifactId.dao.entity.api.IMenuItem;
 import groupId.artifactId.exceptions.DaoException;
 import groupId.artifactId.exceptions.NoContentException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.OptimisticLockException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -83,6 +85,9 @@ public class MenuDao implements IMenuDao {
             }
             currentEntity.setName(menu.getName());
             currentEntity.setEnable(menu.getEnable());
+            if (menu.getItems() != null && !menu.getItems().isEmpty()) {
+                currentEntity.setItems(menu.getItems());
+            }
             entityTransaction.merge(currentEntity);
             return currentEntity;
         } catch (NoContentException e) {
@@ -95,6 +100,30 @@ public class MenuDao implements IMenuDao {
                         + menu);
             } else {
                 throw new DaoException("Failed to update menu" + menu + " by id:" + id + "\t cause" + e.getMessage(), e);
+            }
+        }
+    }
+
+    @Override
+    public IMenu updateItems(IMenu menu, IMenuItem menuItem, EntityManager entityTransaction) {
+        try {
+            List<IMenuItem> items = menu.getItems();
+            items.add(menuItem);
+            Menu currentEntity = (Menu) menu;
+            currentEntity.setItems(items);
+            entityTransaction.merge(currentEntity);
+            return currentEntity;
+        } catch (NoContentException e) {
+            throw new NoContentException(e.getMessage());
+        } catch (OptimisticLockException e) {
+            throw new OptimisticLockException("menu table update failed, version does not match update denied");
+        } catch (Exception e) {
+            if (e.getMessage().contains(MENU_UK)) {
+                throw new NoContentException("menu table update failed,  check preconditions and FK values: "
+                        + menu);
+            } else {
+                throw new DaoException("Failed to update menu" + menu + " with MenuItem:" + menuItem + "\t cause" +
+                        e.getMessage(), e);
             }
         }
     }
@@ -116,7 +145,8 @@ public class MenuDao implements IMenuDao {
         }
     }
 
-    private IMenu getLock(Long id, EntityManager entityTransaction) {
+    @Override
+    public IMenu getLock(Long id, EntityManager entityTransaction) {
         try {
             Menu menu = entityTransaction.find(Menu.class, id);
             if (menu == null) {
--- src/main/java/groupId/artifactId/dao/api/IMenuDao.java
+++ src/main/java/groupId/artifactId/dao/api/IMenuDao.java
@@ -1,7 +1,12 @@
 package groupId.artifactId.dao.api;
 
 import groupId.artifactId.dao.entity.api.IMenu;
+import groupId.artifactId.dao.entity.api.IMenuItem;
+
+import javax.persistence.EntityManager;
 
 public interface IMenuDao extends IDao<IMenu>, IDaoUpdate<IMenu>, IDaoDelete {
+    IMenu updateItems(IMenu menu, IMenuItem menuItem, EntityManager entityTransaction);
+    IMenu getLock(Long id, EntityManager entityTransaction);
 }
 
