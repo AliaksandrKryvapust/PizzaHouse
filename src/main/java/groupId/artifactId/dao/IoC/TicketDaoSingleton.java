89d5c24420b91afafe38028d82df42d83399414e
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-11-29 20:50:59 +0100

    new Ticket Dao IoC entities Hibernate

--- src/main/java/groupId/artifactId/dao/IoC/TicketDaoSingleton.java
+++ src/main/java/groupId/artifactId/dao/IoC/TicketDaoSingleton.java
@@ -1,21 +1,15 @@
 package groupId.artifactId.dao.IoC;
 
 import groupId.artifactId.dao.TicketDao;
-import groupId.artifactId.dao.api.DataSourceCreator;
+import groupId.artifactId.dao.api.EntityManagerFactoryHibernate;
 import groupId.artifactId.dao.api.ITicketDao;
-
-import java.beans.PropertyVetoException;
 
 public class TicketDaoSingleton {
     private final ITicketDao ticketDao;
     private volatile static TicketDaoSingleton instance = null;
 
     public TicketDaoSingleton() {
-        try {
-            this.ticketDao = new TicketDao(DataSourceCreator.getInstance());
-        } catch (PropertyVetoException e) {
-            throw new RuntimeException("Unable to get Data Source class for TicketDao", e);
-        }
+        this.ticketDao = new TicketDao(EntityManagerFactoryHibernate.getEntityManager());
     }
 
     public static ITicketDao getInstance() {
