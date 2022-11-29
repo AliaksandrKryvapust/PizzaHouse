465e2e011ce466993caee259a4160030fd17364e
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-11-29 21:40:44 +0100

    new Order Service IoC for entities Hibernate

--- src/main/java/groupId/artifactId/service/IoC/OrderServiceSingleton.java
+++ src/main/java/groupId/artifactId/service/IoC/OrderServiceSingleton.java
@@ -1,9 +1,9 @@
 package groupId.artifactId.service.IoC;
 
 import groupId.artifactId.core.mapper.*;
-import groupId.artifactId.dao.IoC.OrderDaoSingleton;
-import groupId.artifactId.dao.IoC.SelectedItemDaoSingleton;
 import groupId.artifactId.dao.IoC.TicketDaoSingleton;
+import groupId.artifactId.dao.api.EntityManagerFactoryHibernate;
+import groupId.artifactId.service.MenuItemService;
 import groupId.artifactId.service.OrderService;
 import groupId.artifactId.service.api.IOrderService;
 
@@ -12,10 +12,10 @@ public class OrderServiceSingleton {
     private volatile static OrderServiceSingleton instance = null;
 
     public OrderServiceSingleton() {
-        this.orderService = new OrderService(SelectedItemDaoSingleton.getInstance(), OrderDaoSingleton.getInstance(),
-                TicketDaoSingleton.getInstance(), OrderDataServiceSingleton.getInstance(), new TicketMapper(new OrderMapper(
+        this.orderService = new OrderService(TicketDaoSingleton.getInstance(), OrderDataServiceSingleton.getInstance(),
+                MenuItemServiceSingleton.getInstance(), EntityManagerFactoryHibernate.getEntityManager(), new TicketMapper(new OrderMapper(
                 new SelectedItemMapper(new MenuItemMapper(new PizzaInfoMapper())))), new OrderMapper(new SelectedItemMapper(
-                new MenuItemMapper(new PizzaInfoMapper()))));
+                new MenuItemMapper(new PizzaInfoMapper()))), new SelectedItemMapper(new MenuItemMapper(new PizzaInfoMapper())));
     }
 
     public static IOrderService getInstance() {
