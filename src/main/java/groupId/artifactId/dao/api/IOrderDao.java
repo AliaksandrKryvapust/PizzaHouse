89c0baf41402fd2476896cfd32d7426f10fec13c
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-11-29 20:47:58 +0100

    Dao for selected Item and Order ware removed

--- src/main/java/groupId/artifactId/dao/api/IOrderDao.java
+++
@@ -1,8 +0,0 @@
-package groupId.artifactId.dao.api;
-
-import groupId.artifactId.dao.entity.api.IOrder;
-
-public interface IOrderDao extends IDao<IOrder>, IDaoDelete{
-    IOrder getAllData(Long id);
-    Boolean exist(Long id);
-}
--- src/main/java/groupId/artifactId/dao/api/ISelectedItemDao.java
+++
@@ -1,9 +0,0 @@
-package groupId.artifactId.dao.api;
-
-import groupId.artifactId.dao.entity.api.ISelectedItem;
-
-public interface ISelectedItemDao extends IDao<ISelectedItem>, IDaoDelete {
-    ISelectedItem getAllData(Long id);
-
-    Boolean exist(Long id);
-}
