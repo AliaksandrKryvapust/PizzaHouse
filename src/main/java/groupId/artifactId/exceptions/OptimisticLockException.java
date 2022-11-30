af29033df263dd993037bbe764b169d880db0dcb
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-11-30 18:53:01 +0100

    Optimistic lock exception was removed

--- src/main/java/groupId/artifactId/exceptions/OptimisticLockException.java
+++
@@ -1,7 +0,0 @@
-package groupId.artifactId.exceptions;
-
-public class OptimisticLockException extends RuntimeException {
-    public OptimisticLockException(String errorMsg) {
-        super(errorMsg);
-    }
-}
