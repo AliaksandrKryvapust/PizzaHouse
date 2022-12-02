d840eb19285343670b9aaee1d7ec5cae7ced29c8
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-02 11:39:24 +0100

    completed order Dao IoC for hibernate

--- src/main/java/groupId/artifactId/dao/IoC/CompletedOrderDaoSingleton.java
+++ src/main/java/groupId/artifactId/dao/IoC/CompletedOrderDaoSingleton.java
@@ -1,21 +1,15 @@
 package groupId.artifactId.dao.IoC;
 
 import groupId.artifactId.dao.CompletedOrderDao;
-import groupId.artifactId.dao.api.DataSourceCreator;
+import groupId.artifactId.dao.api.EntityManagerFactoryHibernate;
 import groupId.artifactId.dao.api.ICompletedOrderDao;
-
-import java.beans.PropertyVetoException;
 
 public class CompletedOrderDaoSingleton {
     private final ICompletedOrderDao completedOrderDao;
     private volatile static CompletedOrderDaoSingleton instance = null;
 
     public CompletedOrderDaoSingleton() {
-        try {
-            this.completedOrderDao = new CompletedOrderDao(DataSourceCreator.getInstance());
-        } catch (PropertyVetoException e) {
-            throw new RuntimeException("Unable to get Data Source class for CompletedOrderDao", e);
-        }
+        this.completedOrderDao = new CompletedOrderDao(EntityManagerFactoryHibernate.getEntityManager());
     }
 
     public static ICompletedOrderDao getInstance() {
