20ee2d2f155d1dbc91117ef619c1630bb90948a7
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-04 16:50:05 +0100

    Pizza Info Mapper was changed for Spring context

--- src/main/java/groupId/artifactId/core/mapper/PizzaInfoMapper.java
+++ src/main/java/groupId/artifactId/core/mapper/PizzaInfoMapper.java
@@ -4,7 +4,12 @@ import groupId.artifactId.core.dto.input.PizzaInfoDtoInput;
 import groupId.artifactId.core.dto.output.PizzaInfoDtoOutput;
 import groupId.artifactId.dao.entity.PizzaInfo;
 import groupId.artifactId.dao.entity.api.IPizzaInfo;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
 
+@Component
+@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class PizzaInfoMapper {
     public PizzaInfoMapper() {
     }
