10adb96f3cba5e16e13f2274dadb13788897a7ef
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-04 18:09:24 +0100

    Order Stage Mapper was modified for Spring context

--- src/main/java/groupId/artifactId/core/mapper/OrderStageMapper.java
+++ src/main/java/groupId/artifactId/core/mapper/OrderStageMapper.java
@@ -3,7 +3,12 @@ package groupId.artifactId.core.mapper;
 import groupId.artifactId.core.dto.output.OrderStageDtoOutput;
 import groupId.artifactId.dao.entity.OrderStage;
 import groupId.artifactId.dao.entity.api.IOrderStage;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
 
+@Component
+@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class OrderStageMapper {
     public OrderStageMapper() {
     }
