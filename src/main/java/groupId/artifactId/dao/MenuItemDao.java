6a5e2dc5647a867247b156dfe382835222a93c6a
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-04 16:43:00 +0100

    Menu Item Dao was modified for Spring context

--- src/main/java/groupId/artifactId/dao/MenuItemDao.java
+++ src/main/java/groupId/artifactId/dao/MenuItemDao.java
@@ -6,17 +6,21 @@ import groupId.artifactId.dao.entity.PizzaInfo;
 import groupId.artifactId.dao.entity.api.IMenuItem;
 import groupId.artifactId.exceptions.DaoException;
 import groupId.artifactId.exceptions.NoContentException;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.OptimisticLockException;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static groupId.artifactId.core.Constants.*;
 
+@Repository
 public class MenuItemDao implements IMenuItemDao {
     private static final String SELECT_MENU_ITEM = "SELECT menuItem from MenuItem menuItem ORDER BY id";
     private static final String SELECT_MENU_ITEMS_BY_IDS = "SELECT menuItem FROM MenuItem menuItem WHERE menuItem.id IN :ids";
+    @PersistenceContext
     private final EntityManager entityManager;
 
     public MenuItemDao(EntityManager entityManager) {
