7a6565534848c6f816c96978ad7769dffca398b5
Aliaksandr Kryvapust <alexandrkryvopust@gmail.com>
2022-12-04 18:09:12 +0100

    Order Mapper was modified for Spring context

--- src/main/java/groupId/artifactId/core/mapper/OrderMapper.java
+++ src/main/java/groupId/artifactId/core/mapper/OrderMapper.java
@@ -4,14 +4,19 @@ import groupId.artifactId.core.dto.output.OrderDtoOutput;
 import groupId.artifactId.core.dto.output.SelectedItemDtoOutput;
 import groupId.artifactId.dao.entity.api.IOrder;
 import groupId.artifactId.dao.entity.api.ISelectedItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Component
+@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class OrderMapper {
 
     private final SelectedItemMapper selectedItemMapper;
-
+    @Autowired
     public OrderMapper(SelectedItemMapper selectedItemMapper) {
         this.selectedItemMapper = selectedItemMapper;
     }
